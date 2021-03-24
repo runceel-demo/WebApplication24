dd23f4a4017046530994aea2dc4337b502128143
Kazuki Ota <kaota@microsoft.com>
2021-03-24 10:09:40 +0900

    xxxxxxxx

--- WebApplication24/Startup.cs
+++ WebApplication24/Startup.cs
@@ -26,7 +26,6 @@ namespace WebApplication24
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +35,6 @@ namespace WebApplication24
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
